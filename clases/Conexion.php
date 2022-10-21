777dc8079b7c80b74aeb1a847150c7651ea9a6c7
YesicaNRojasH <yesica.w.w.nrh65@gmail.com>
2022-10-21 06:37:52 -0500

    nuevos cambios

---
+++ clases/Conexion.php
@@ -0,0 +1,19 @@
+<?php 
+
+	class conectar{
+		private $servidor="bdmysql.testing-apps.com";
+		private $usuario="grupo7";
+		private $password="h%7E5qM^Bh8!";
+		private $bd="grupo7";
+
+		public function conexion(){
+			$conexion=mysqli_connect($this->servidor,
+									 $this->usuario,
+									 $this->password,
+									 $this->bd);
+			return $conexion;
+		}
+	}
+
+
+?>
